e7060192c06e8d91bfbf063bba2a29c8eb28bbbc
DmitriGanin <sjimmie13@gmail.com>
2025-02-23 12:27:27 +0200

    menu branch + file structure

--- booking_system.py
+++ booking_system.py
@@ -2,7 +2,27 @@
 # It will import functions created by student from other modules (.py files) and will use those in the main code
 import user_menu
 
+menu_list = user_menu.menu_list # for convenience of others the menu list (and its index as an option digit) is available in main .py code
+
 def launch_booking_system():
-    pass
+    """
+    Initiates the main loop of menu selection for chosen actions until Exit is chosen
+    """
+    print("\nWelcome to the Appointment Booking System!")
+    while True:
+        user_option = user_menu.user_menu()
+        if user_option == 0:
+            print("\nGood bye!\n")
+            exit()
+        elif user_option == 1:  # Schedule new appointment(s)
+            pass # placeholder for the modular function
+        elif user_option == 2:  # View upcoming appointments
+            pass # placeholder for the modular function
+        elif user_option == 3:  # Cancel or reschedule an appointment
+            pass # placeholder for the modular function
+        elif user_option == 4:  # Filter appointments by date or client name
+            pass # placeholder for the modular function
+        elif user_option == 5:  # Save and load appointment data
+            pass # placeholder for the modular function
 
 launch_booking_system()
--- user_menu.py
+++ user_menu.py
@@ -1,5 +1,32 @@
 # This is a User Menu module that handles how user menu looks like
 # Created by Dmitri Ganin
 
+menu_list = [
+    "Exit",
+    "Schedule new appointment(s)", 
+    "View upcoming appointments", 
+    "Cancel or reschedule an appointment", 
+    "Filter appointments by date or client name", 
+    "Save and load appointment data"
+    ]
+
+# to verify the string option selected by user against menu option number without potential type casting error
+menu_index_as_str = [str(menu_list.index(item)) for item in menu_list]
+
 def user_menu():
-    pass
+    """
+    The function shows static user menu and selection of options
+    Args: none
+    Returns: selected user option as a digit
+    """
+    print("\nPlease choose an optin from the list:")
+    for item in menu_list:
+        if item != menu_list[0]:
+            print(menu_list.index(item), '-', item)
+    print(menu_list.index(menu_list[0]), '-', menu_list[0])
+    while True:
+        user_option = input("\nEnter the user option you want using one of digits above: ")
+        if user_option.strip()[0] in menu_index_as_str:
+            return int(user_option.strip()[0])
+        else:
+            print('Your input is not valid, please try again.')
