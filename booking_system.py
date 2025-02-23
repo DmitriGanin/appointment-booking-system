c054d9672563eddb8261957575173be3dec9fefb
DmitriGanin <sjimmie13@gmail.com>
2025-02-23 09:28:48 +0200

    initial modular structure creation

--- booking_system.py
+++ booking_system.py
@@ -1,1 +1,8 @@
+# This is a main file for launching the appointment booking system
+# It will import functions created by student from other modules (.py files) and will use those in the main code
+import user_menu
 
+def launch_booking_system():
+    pass
+
+launch_booking_system()
--- user_menu.py
+++ user_menu.py
@@ -1,1 +1,5 @@
+# This is a User Menu module that handles how user menu looks like
+# Created by Dmitri Ganin
 
+def user_menu():
+    pass
